a15e7f330b28e6e371fc40fbd68e42a28002ada1
Aditya Dwi Cahyono <73687000+adityadwic@users.noreply.github.com>
2024-10-28 11:48:16 +0700

    Change Name

---
+++ pishoot-development/Pixshoot/PixshootApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixshootApp.swift
+//  Pixshoot
+//
+//  Created by Aditya Dwi Cahyono on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PixshootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
