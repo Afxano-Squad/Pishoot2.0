4c0a4a95da62c4fc5f77817654c1147966439dd3
Farid Andika <faridandika159@gmail.com>
2024-11-15 10:36:39 +0700

    changed the grid view from full outline square into removing the outline except its corners

--- pishoot-development/Pixshoot/Views/RuleOf3GridView.swift
+++ pishoot-development/Pixshoot/Views/RuleOf3GridView.swift
@@ -5,51 +5,62 @@
 //  Created by Farid Andika on 30/10/24.
 //
 
+
 import SwiftUI
 
 struct RuleOf3GridView: View {
- 
-        
-        var lineColor: Color = .white
-        var lineWidth: CGFloat = 1
-        
-        var body: some View {
-            GeometryReader { geometry in
-                Path { path in
-                    let width = geometry.size.width
-                    let height = geometry.size.height
-                    
-                    // Horizontal Lines (Rule of Thirds)
-                    path.move(to: CGPoint(x: 0, y: height / 3))
-                    path.addLine(to: CGPoint(x: width, y: height / 3))
-                    
-                    path.move(to: CGPoint(x: 0, y: 2 * height / 3))
-                    path.addLine(to: CGPoint(x: width, y: 2 * height / 3))
-                    
-                    // Vertical Lines (Rule of Thirds)
-                    path.move(to: CGPoint(x: width / 3, y: 0))
-                    path.addLine(to: CGPoint(x: width / 3, y: height))
-                    
-                    path.move(to: CGPoint(x: 2 * width / 3, y: 0))
-                    path.addLine(to: CGPoint(x: 2 * width / 3, y: height))
-                    
-                   
-                    path.move(to: CGPoint(x: 0, y: 0))
-                    path.addLine(to: CGPoint(x: width, y: 0))
-                    
-                    // Bottom Border
-                    path.move(to: CGPoint(x: 0, y: height))
-                    path.addLine(to: CGPoint(x: width, y: height))
-                    
-                    // Left Border
-                    path.move(to: CGPoint(x: 0, y: 0))
-                    path.addLine(to: CGPoint(x: 0, y: height))
-                    
-                    // Right Border
-                    path.move(to: CGPoint(x: width, y: 0))
-                    path.addLine(to: CGPoint(x: width, y: height))
-                }
-                .stroke(lineColor, lineWidth: lineWidth)
+    
+    var lineColor: Color = .white
+    var lineWidth: CGFloat = 1
+    
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                let cornerLength: CGFloat = 20  // Panjang garis sudut
+                
+                // Horizontal Lines (Rule of Thirds)
+                path.move(to: CGPoint(x: 0, y: height / 3))
+                path.addLine(to: CGPoint(x: width, y: height / 3))
+                
+                path.move(to: CGPoint(x: 0, y: 2 * height / 3))
+                path.addLine(to: CGPoint(x: width, y: 2 * height / 3))
+                
+                // Vertical Lines (Rule of Thirds)
+                path.move(to: CGPoint(x: width / 3, y: 0))
+                path.addLine(to: CGPoint(x: width / 3, y: height))
+                
+                path.move(to: CGPoint(x: 2 * width / 3, y: 0))
+                path.addLine(to: CGPoint(x: 2 * width / 3, y: height))
+                
+                // Corner Lines (Only short lines at each corner)
+                // Top Left Corner
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: cornerLength, y: 0))
+                path.move(to: CGPoint(x: 0, y: 0))
+                path.addLine(to: CGPoint(x: 0, y: cornerLength))
+                
+                // Top Right Corner
+                path.move(to: CGPoint(x: width - cornerLength, y: 0))
+                path.addLine(to: CGPoint(x: width, y: 0))
+                path.move(to: CGPoint(x: width, y: 0))
+                path.addLine(to: CGPoint(x: width, y: cornerLength))
+                
+                // Bottom Left Corner
+                path.move(to: CGPoint(x: 0, y: height - cornerLength))
+                path.addLine(to: CGPoint(x: 0, y: height))
+                path.move(to: CGPoint(x: 0, y: height))
+                path.addLine(to: CGPoint(x: cornerLength, y: height))
+                
+                // Bottom Right Corner
+                path.move(to: CGPoint(x: width - cornerLength, y: height))
+                path.addLine(to: CGPoint(x: width, y: height))
+                path.move(to: CGPoint(x: width, y: height))
+                path.addLine(to: CGPoint(x: width, y: height - cornerLength))
+                
             }
+            .stroke(lineColor, lineWidth: lineWidth)
         }
     }
+}
